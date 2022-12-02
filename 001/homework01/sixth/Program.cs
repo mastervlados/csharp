f25df954c52f8080f8122ac8ae5c03d7a92801c4
Vladislav Elizarov <velizarov95@yandex.ru>
2022-12-02 09:34:18 -0500

    The sixth task is done!

---
+++ 001/homework01/sixth/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+Console.WriteLine("Программа покажет четное число или нет");
+
+int number = 0;
+
+Console.WriteLine(""); // br
+Console.Write("Введите число: ");
+if(!int.TryParse(Console.ReadLine(), out number))
+{
+    Console.WriteLine("Нужно ввести число!");
+}
+else
+{
+    if(number % 2 == 0)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
