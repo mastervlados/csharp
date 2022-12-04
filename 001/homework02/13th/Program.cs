b0b27e82de452c8ad1b72f24397a22813f9f1f1e
Vladislav Elizarov <velizarov95@yandex.ru>
2022-12-04 06:51:14 -0500

    Add second homework for 001

---
+++ 001/homework02/13th/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+void ShowThirdNumberOfNumber(int number)
+{
+    int thirdNumber = -1;
+
+    if(number >= 100)
+    {
+        while (number > 999)
+        {
+            number = number / 10;
+        }
+
+        thirdNumber = number % 10;
+
+        Console.WriteLine($"Третья цифра = {thirdNumber}");
+    }
+    else 
+        Console.WriteLine($"У числа {number} - третьей цифры нет!");
+}
+
+ShowThirdNumberOfNumber(645);
+ShowThirdNumberOfNumber(78);
+ShowThirdNumberOfNumber(32679);
+ShowThirdNumberOfNumber(3297932);
+ShowThirdNumberOfNumber(999);
---
+++ 001/homework02/15th/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+// day off - это выходной день
+void IsThisDayOff(int dayNumber)
+{
+    string[] days = new string[8] {
+        "Дни недели", // Title 
+        "Понедельник", // 1
+        "Вторник", // 2
+        "Среда",  // 3
+        "Четверг", // 4
+        "Пятница", // 5
+        "Суббота", // 6
+        "Воскресенье" // 7
+    };
+
+    if(dayNumber == 6 || dayNumber == 7)
+        Console.WriteLine($"Да, {days[dayNumber]} является выходным днем.");
+    else if(dayNumber >= 1 && dayNumber <= 5) // [1, 5]
+        Console.WriteLine($"Нет, {days[dayNumber]} является рабочим днем.");
+    else
+        Console.WriteLine($"{days[0]}: в следующий раз напишите порядковый номер.");
+}
+
+IsThisDayOff(6);
+IsThisDayOff(7);
+IsThisDayOff(1);
+IsThisDayOff(8);
+IsThisDayOff(-6);
