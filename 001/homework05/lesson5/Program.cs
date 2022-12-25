72df2ed781f76f8eaf3eaeb6c7d81bc3ea8602eb
Vladislav Elizarov <velizarov95@yandex.ru>
2022-12-25 03:05:55 -0500

    Init homeworks tasks! 05-07-08 Hell Done!

--- 001/homework05/lesson5/Program.cs
+++ 001/homework05/lesson5/Program.cs
@@ -1,7 +1,11 @@
-﻿// Lesson 5
+﻿// ****************
+// *** Lesson 5 ***
+// ****************
 
 
-
-
-
-
+// UPDATE. Start from Lesson 5 finish at Lesson 8
+// Add Lesson 5's example
+// Add homework for Lesson 7
+// Add Lesson 8's example & homework
+// I had doing my own MVP project.
+// Developing a design in Figma
---
+++ 001/homework07/lesson7/Program.cs
@@ -0,0 +1,69 @@
+﻿// ***********************
+// *** Lesson 7 Matrix ***
+// ***********************
+// int[,] = matrix = new int[rows, colums]
+// matrix.GetLength(0) // [0, 2], [1, 3], [2, 4], etc
+// matrix{i,j}
+
+
+//Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+//m = 3, n = 4.
+//1 4 8 19
+//5 -2 33 -2
+//77 3 8 1
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    Console.WriteLine();
+
+    return result;
+}
+
+int[,] InitMatrix(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
+
+int rows = GetNumber("Введите количество строк: ");
+int columns = GetNumber("Введите количество столбцов: ");
+int[,] matrix = InitMatrix(rows, columns);
+PrintMatrix(matrix);
+
