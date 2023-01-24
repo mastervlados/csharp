88196126e7003e092f36e2c5d5510106e8c01d43
Vladislav Elizarov <velizarov95@yandex.ru>
2023-01-23 19:27:27 -0500

    Init task, add folder 'assets', add README

--- 001/finalwork/Program.cs
+++ 001/finalwork/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// ********************************
+// **** Final Work (Developer) ****
+// ********************************
+/*
+ * 1. Create Git repo
+ * 2. Complete the circuit of the algorithm
+ * 3. Add folder «assets» to that repo, add file README.md
+ * 4. Code | result array[] | array[v].length <= 3 :: string; uses arrays
+ *      Examples:
+ *          input: ["hell", "2", "done", ":D"] output: ["2", ":D"]
+ *          input: ["1337", "322", "-2", "computer science"] output: ["322", "-2"]
+ *          input: ["Russia", "Indonesia", "Denmark"] output: []
+ * 5. Do this by Git control system and post it to GitHub
+ * 6. Generate pdf file of this project
+ */
